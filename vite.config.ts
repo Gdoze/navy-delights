01ebb218cb6350238e820c8b9eaada48efcb0daf
Gdoze <66882739+Gdoze@users.noreply.github.com>
2026-04-10 17:24:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/le-navy/'  // ← remplacez par votre nom de dépôt
+  base: '/navy-delights/',
 })
